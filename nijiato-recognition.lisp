8a7202a779970eeae6b7551b63198fdbcf4c048f
Ulric Wilfred <shaman.sir@gmail.com>
2010-07-20 23:06:21 +0300

    small correction

--- nijiato-recognition.lisp
+++ nijiato-recognition.lisp
@@ -53,7 +53,7 @@
 
 ;; fingers-deltas                              r    g    b
 (defvar *fingers-deltas* (list :thumb  (vector 0.09 0.07 0.04)
-                               :index  (vector 0.06 0.04 0.03)
+                               :index  (vector 0.04 0.05 0.05)
                                :middle (vector 0.02 0.02 0.01)
                                :ring   (vector 0.04 0.05 0.05)
                                :little (vector 0.01 0.01 0.02)))
